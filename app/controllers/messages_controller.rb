cdd3a5e3914fe30ba44e12b76768ac8740c4ef9f
pdkproitf <phucdaica.pro@gmail.com>
2017-07-05 17:03:50 +0700

    send a messages and show to view

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,4 +1,19 @@
 class MessagesController < ApplicationController
   def show
   end
+
+  def create
+    @conversation = Conversation.includes(:recipient).find(params[:conversation_id])
+    @message = @conversation.messages.create(message_params)
+
+    respond_to do |format|
+      format.js
+    end
+  end
+
+  private
+
+  def message_params
+    params.require(:message).permit(:user_id, :body)
+  end
 end
